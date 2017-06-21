252a9d27b40d3214a88ba24f45cf2e55e2351995
Matthew Rice <matthewrice11@yahoo.com>
2017-06-21 00:49:08 -0700

    Add Search component

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
+var TodoSearch = require('TodoSearch');
 
 var TodoApp = React.createClass({
   getInitialState: function () {
@@ -19,18 +20,26 @@ var TodoApp = React.createClass({
         id: 4,
         text: 'Take online course'
         }
-      ]
+      ],
+      showCompleted: false,
+      searchText:''
     };
   },
 handleAddTodo: function(text) {
     alert('New Todo: '+text);
   },
-
+handleSearch: function(showCompleted, searchText){
+  this.setState({
+    showCompleted: showCompleted,
+    searchText: searchText.toLowerCase()
+  })
+},
   render: function (){
     var {todos, newTodo} = this.state;
 
     return(
       <div>
+        <TodoSearch onSearch={this.handleSearch}/>
         <TodoList todos={todos}/>
         <AddTodo onSubmit={this.handleAddTodo}/>
       </div>
